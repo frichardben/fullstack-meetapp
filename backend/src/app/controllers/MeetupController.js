053d80994d13990046f11b0cfdcf33a85d68ac94
Richard Ben <frichardben@yahoo.com.br>
2020-03-26 16:47:16 -0300

    feat: validating input data

--- backend/src/app/controllers/MeetupController.js
+++ backend/src/app/controllers/MeetupController.js
@@ -1,17 +1,66 @@
+import * as Yup from 'yup';
+import { isBefore, parseISO } from 'date-fns';
 import Meetup from '../models/Meetup';
 
 class MeetupController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      file_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res
+        .status(400)
+        .json({ error: `You can't create a meetup on a past date` });
+    }
+
     try {
       const meetup = await Meetup.create({
         user_id: req.userId,
         ...req.body,
       });
 
-      return res.status(201).json(meetup);
+      return res.json(meetup);
     } catch (error) {
-      return res.status(400).json(error.message);
+      return res.json(error.message);
     }
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string(),
+      description: Yup.string(),
+      location: Yup.string(),
+      date: Yup.date(),
+      file_id: Yup.number(),
+    });
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const user_id = req.userId;
+
+    const meetup = await Meetup.findByPk(req.params.id);
+
+    if (meetup.user_id !== user_id) {
+      return res.status(401).json({ error: 'Not authorized.' });
+    }
+
+    if (isBefore(parseISO(req.body.date), new Date())) {
+      return res.status(400).json({ error: 'Meetup date invalid' });
+    }
+
+    await meetup.update(req.body);
+
+    return res.json(meetup);
   }
 }
 
