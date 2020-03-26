4deb5ddbf5c72b587baa28d6f02ac74c6edeeb73
Richard Ben <frichardben@yahoo.com.br>
2020-03-26 18:47:57 -0300

    feat: list & remove

--- backend/src/app/controllers/MeetupController.js
+++ backend/src/app/controllers/MeetupController.js
@@ -1,6 +1,8 @@
 import * as Yup from 'yup';
 import { isBefore, parseISO } from 'date-fns';
 import Meetup from '../models/Meetup';
+import User from '../models/User';
+import File from '../models/File';
 
 class MeetupController {
   async store(req, res) {
@@ -34,6 +36,32 @@ class MeetupController {
     }
   }
 
+  async index(req, res) {
+    try {
+      const where = {};
+
+      const meetups = await Meetup.findAll({
+        where,
+        include: [
+          {
+            model: User,
+            as: 'organizer',
+            attributes: ['id', 'name', 'email'],
+          },
+          {
+            model: File,
+            as: 'banner',
+            attributes: ['name', 'path', 'url'],
+          },
+        ],
+      });
+
+      return res.json(meetups);
+    } catch (error) {
+      return res.status(400).json(error);
+    }
+  }
+
   async update(req, res) {
     const schema = Yup.object().shape({
       title: Yup.string(),
@@ -42,25 +70,48 @@ class MeetupController {
       date: Yup.date(),
       file_id: Yup.number(),
     });
+
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({ error: 'Validation fails' });
     }
 
-    const user_id = req.userId;
+    try {
+      const user_id = req.userId;
 
-    const meetup = await Meetup.findByPk(req.params.id);
+      const meetup = await Meetup.findByPk(req.params.id);
 
-    if (meetup.user_id !== user_id) {
-      return res.status(401).json({ error: 'Not authorized.' });
+      if (meetup.user_id !== user_id) {
+        return res.status(401).json({ error: 'Not authorized.' });
+      }
+
+      if (isBefore(parseISO(req.body.date), new Date())) {
+        return res.status(400).json({ error: 'Meetup date invalid' });
+      }
+
+      await meetup.update(req.body);
+
+      return res.json(meetup);
+    } catch (error) {
+      return res.status(400).json(error);
     }
+  }
 
-    if (isBefore(parseISO(req.body.date), new Date())) {
-      return res.status(400).json({ error: 'Meetup date invalid' });
+  async delete(req, res) {
+    try {
+      const user_id = req.userId;
+
+      const meetup = await Meetup.findByPk(req.params.id);
+
+      if (meetup.user_id !== user_id) {
+        return res.status(401).json({ error: 'Not authorized.' });
+      }
+
+      await meetup.destroy();
+
+      return res.send();
+    } catch (error) {
+      return res.status(400).json(error);
     }
-
-    await meetup.update(req.body);
-
-    return res.json(meetup);
   }
 }
 
--- backend/src/app/models/Meetup.js
+++ backend/src/app/models/Meetup.js
@@ -17,8 +17,8 @@ class Meetup extends Model {
   }
 
   static associate(models) {
-    this.belongsTo(models.User, { foreignKey: 'user_id' });
-    this.belongsTo(models.File, { foreignKey: 'file_id' });
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'organizer' });
+    this.belongsTo(models.File, { foreignKey: 'file_id', as: 'banner' });
   }
 }
 export default Meetup;
