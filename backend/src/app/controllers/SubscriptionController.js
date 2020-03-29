34bb9a195ca6327cf4de28f59cd44c9e09f18712
Richard Ben <frichardben@yahoo.com.br>
2020-03-29 12:31:30 -0300

    feat: adding subscription

---
+++ backend/src/app/controllers/SubscriptionController.js
@@ -0,0 +1,83 @@
+import { Op } from 'sequelize';
+import Subscription from '../models/Subscription';
+import User from '../models/User';
+import Meetup from '../models/Meetup';
+
+class SubscriptionController {
+  async store(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+      const meetup = await Meetup.findByPk(req.params.id);
+
+      if (meetup.user_id === req.userId) {
+        return res
+          .status(400)
+          .json({ error: "Can't subscribe to you own meetups." });
+      }
+
+      if (meetup.past) {
+        return res
+          .status(400)
+          .json({ error: "Can't subscribe to past meetups" });
+      }
+
+      const checkDate = await Subscription.findOne({
+        where: {
+          user_id: user.id,
+        },
+        include: [
+          {
+            model: Meetup,
+            required: true,
+            where: {
+              date: meetup.date,
+            },
+          },
+        ],
+      });
+
+      if (checkDate) {
+        return res
+          .status(400)
+          .json({ error: "Can't subscribe to two meetups at the same time" });
+      }
+
+      const subscribe = await Subscription.create({
+        user_id: user.id,
+        meetup_id: meetup.id,
+      });
+
+      return res.json(subscribe);
+    } catch (error) {
+      return res.json(error.message);
+    }
+  }
+
+  async index(req, res) {
+    try {
+      const subscription = await Subscription.findAll({
+        where: {
+          user_id: req.userId,
+        },
+        include: [
+          {
+            model: Meetup,
+            where: {
+              date: {
+                [Op.gt]: new Date(),
+              },
+            },
+            required: true,
+          },
+        ],
+        order: [[Meetup, 'date']],
+      });
+
+      return res.json(subscription);
+    } catch (error) {
+      return res.status(401).json(error.message);
+    }
+  }
+}
+
+export default new SubscriptionController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
 import OrganizerController from './app/controllers/OrganizerController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -28,5 +29,8 @@ routes.put('/meetups/:id', MeetupController.update);
 routes.delete('/meetups/:id', MeetupController.delete);
 
 routes.get('/organizers', OrganizerController.index);
+
+routes.get('/subscriptions', SubscriptionController.index);
+routes.post('/meetups/:id/subscriptions', SubscriptionController.store);
 
 export default routes;
