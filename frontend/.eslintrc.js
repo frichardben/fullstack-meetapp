2a4e95fa01c9da77c77d65ab17f54f0010759991
Richard Ben <frichardben@yahoo.com.br>
2020-04-19 21:31:32 -0300

    chore: root import

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
+  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
     'prettier/prettier': 'error',
     'react/jsx-filename-extension': [
@@ -28,5 +28,22 @@ module.exports = {
        { extensions: ['.jsx', '.js'] }
     ],
     'import/prefer-default-export': 'off',
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "react/jsx-one-expression-per-line": "off",
+    "global-require": "off",
+    "react-native/no-raw-text": "off",
+    "no-param-reassign": "off",
+    "no-underscore-dangle": "off",
+    camelcase: "off",
+    "no-console": ["error", { allow: ["tron"] }],
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      }
+    }
   },
 };
