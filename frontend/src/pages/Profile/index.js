b34a367bc9f72958cc884080fbcb841c29da266b
Richard Ben <frichardben@yahoo.com.br>
2020-04-27 20:14:28 -0300

    style: profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { MdAddCircleOutline } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  return (
+    <Container>
+      <Form>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" placeholder="Seu endereço de e-mail" />
+
+        <hr />
+
+        <Input type="password" name="oldPassword" placeholder="Senha atual" />
+        <Input type="password" name="password" placeholder="Nova senha" />
+        <Input
+          type="password"
+          name="confirmPassword"
+          placeholder="Confirmação de senha"
+        />
+
+        <button type="submit">
+          <MdAddCircleOutline
+            color="#fff"
+            style={{ marginRight: 10 }}
+            size={20}
+          />
+          Salvar perfil
+        </button>
+      </Form>
+    </Container>
+  );
 }
