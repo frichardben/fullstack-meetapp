25e07a13971e81063e1f7130e3a94d1aa76aa20c
Richard Ben <frichardben@yahoo.com.br>
2020-04-28 23:31:52 -0300

    feat: logout

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { useDispatch } from 'react-redux';
+import { signOut } from '~/store/modules/auth/actions';
+
 import logo from '~/assets/logo.svg';
 
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
@@ -23,7 +32,9 @@ export default function Header() {
             </div>
           </Profile>
 
-          <button type="submit">Sair</button>
+          <button type="button" onClick={handleSignOut}>
+            Sair
+          </button>
         </aside>
       </Content>
     </Container>
