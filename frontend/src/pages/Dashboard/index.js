ed68532ae39fc68cffc3d5b3fac8ca371c908d74
Richard Ben <frichardben@yahoo.com.br>
2020-04-30 17:26:37 -0300

    feat: organizer's meeting list

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { format, parseISO } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import { MdAddCircleOutline, MdChevronRight } from 'react-icons/md';
 
 import { Container, Meetup } from './styles';
 
+import api from '~/services/api';
+
 export default function Dashboard() {
+  const [meetups, setMeetups] = useState([]);
+
+  useEffect(() => {
+    async function loadMeetups() {
+      const response = await api.get('organizers');
+
+      setMeetups(response.data);
+    }
+
+    loadMeetups();
+  }, []);
+
   return (
     <Container>
       <header>
@@ -24,27 +40,24 @@ export default function Dashboard() {
       </header>
 
       <ul>
-        <Meetup>
-          <strong>Reactjs</strong>
-          <span>
-            10 de Agosto
-            <MdChevronRight size={36} color="#fff" style={{ marginLeft: 30 }} />
-          </span>
-        </Meetup>
-        <Meetup>
-          <strong>Reactjs</strong>
-          <span>
-            10 de Agosto
-            <MdChevronRight size={36} color="#fff" style={{ marginLeft: 30 }} />
-          </span>
-        </Meetup>
-        <Meetup>
-          <strong>Reactjs</strong>
-          <span>
-            10 de Agosto
-            <MdChevronRight size={36} color="#fff" style={{ marginLeft: 30 }} />
-          </span>
-        </Meetup>
+        {meetups.map((meetup) => (
+          <Link key={meetup.id} to={`/details/${meetup.id}`}>
+            <Meetup>
+              <strong>{meetup.title}</strong>
+
+              <span>
+                {format(parseISO(meetup.date), "d 'de' MMMM 'às' HH'h' ", {
+                  locale: pt,
+                })}
+                <MdChevronRight
+                  size={36}
+                  color="#fff"
+                  style={{ marginLeft: 30 }}
+                />
+              </span>
+            </Meetup>
+          </Link>
+        ))}
       </ul>
     </Container>
   );
